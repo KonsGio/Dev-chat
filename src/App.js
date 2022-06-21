71fc459ea77f99dab687ebd961a384f194a18095
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-06-21 13:31:39 +0300

    Addition of authToken for Auth component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import {StreamChat} from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import { Cookies } from 'universal-cookie';
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 import './App.css';
 
 
 
 const apiKey = 'c7puk5k7m7e3'
 const client = StreamChat.getInstance(apiKey);
-
+const authToken = false;
 
 const App = () => {
+
+    if(!authToken) return <Auth/>
+
   return (
     <div className="app__wrapper" theme="team light">
       <Chat client={client} theme = "team light">
